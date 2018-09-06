7cc4986d781c0a4eb71f7e42172d57a1c46eda06
José Rodrigues <jyrodrigues@gmail.com>
2018-09-05 21:59:50 -0300

    Add recording

--- src/Main.elm
+++ src/Main.elm
@@ -2,9 +2,10 @@ module Main exposing (Model, Msg(..), init, main, update, view)
 
 import Browser
 import Html exposing (Html, button, div, text)
+import Html.Attributes exposing (style)
 import Html.Events exposing (onClick)
 import Svg exposing (Svg, polyline, svg)
-import Svg.Attributes exposing (fill, height, stroke, viewBox, width)
+import Svg.Attributes exposing (fill, height, stroke, style, viewBox, width)
 
 
 
@@ -37,12 +38,18 @@ type alias State =
 
 type alias Model =
     { state : State
+    , recording : State
+    , recOn : Bool
     }
+
+
+initialState =
+    [ D, L, D, L, D, L, D ]
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( Model []
+    ( Model initialState [] False
     , Cmd.none
     )
 
@@ -55,6 +62,9 @@ type Msg
     = AddD
     | AddL
     | AddR
+    | Backspace
+    | ClearStep
+    | ClearSvg
     | Iterate
 
 
@@ -62,16 +72,33 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         AddD ->
-            ( { model | state = model.state ++ [ D ] }, Cmd.none )
+            ( { model | recording = model.recording ++ [ D ] }, Cmd.none )
 
         AddL ->
-            ( { model | state = model.state ++ [ L ] }, Cmd.none )
+            ( { model | recording = model.recording ++ [ L ] }, Cmd.none )
 
         AddR ->
-            ( { model | state = model.state ++ [ R ] }, Cmd.none )
+            ( { model | recording = model.recording ++ [ R ] }, Cmd.none )
+
+        Backspace ->
+            ( { model
+                | recording =
+                    model.recording
+                        |> List.reverse
+                        |> List.drop 1
+                        |> List.reverse
+              }
+            , Cmd.none
+            )
+
+        ClearStep ->
+            ( { model | recording = [] }, Cmd.none )
+
+        ClearSvg ->
+            ( { model | state = initialState }, Cmd.none )
 
         Iterate ->
-            ( { model | state = apply rule1 model.state }, Cmd.none )
+            ( { model | state = apply (makeRule model.recording) model.state }, Cmd.none )
 
 
 
@@ -87,17 +114,41 @@ subscriptions model =
 -- VIEW
 
 
+mystyle =
+    [ Html.Attributes.style "width" "100%"
+    , Html.Attributes.style "height" "130px"
+    , Html.Attributes.style "border" "1px solid black"
+    ]
+
+
+
+--[ ( "width", "100%" ), ( "height", "120px" ) ]
+
+
 view : Model -> Html Msg
 view model =
     div []
-        [ button [ onClick AddD ] [ text "Add D" ]
-        , button [ onClick AddL ] [ text "Add L" ]
-        , button [ onClick AddR ] [ text "Add R" ]
-        , button [ onClick Iterate ] [ text "Iterate" ]
-        , div [] [ text (String.fromInt <| List.length model.state) ]
+        [ div mystyle
+            [ button [ onClick AddD ] [ text "Add D" ]
+            , button [ onClick AddL ] [ text "Add L" ]
+            , button [ onClick AddR ] [ text "Add R" ]
+            , button [ onClick Backspace ] [ text "Backspace" ]
+            , button [ onClick ClearStep ] [ text "Clear Step" ]
+            , button [ onClick ClearSvg ] [ text "Clear Svg" ]
+            , button [ onClick Iterate ] [ text "Iterate" ]
+            , div [] [ text (String.fromInt <| List.length model.state) ]
+            , div [ Html.Attributes.style "display" "inline-block" ] [ text <| stateToString model.recording ]
+            , div
+                [ Html.Attributes.style "display" "block "
+                ]
+                [ drawSvg model.recording 120 80
+                ]
+            ]
         , div
-            []
-            [ drawSvg model.state
+            [ Html.Attributes.style "display" "block "
+            , Html.Attributes.style "border" "1px solid black"
+            ]
+            [ drawSvg model.state size size
             ]
         ]
 
@@ -106,15 +157,16 @@ view model =
 -- DRAWING
 
 
-drawSvg : State -> Svg Msg
-drawSvg state =
+drawSvg : State -> Float -> Float -> Svg Msg
+drawSvg state w h =
     svg
-        [ width (String.fromFloat size)
-        , height (String.fromFloat size)
-        , viewBox <| "0 0 " ++ String.fromFloat (size / scale) ++ " " ++ String.fromFloat (size / scale)
+        [ width (String.fromFloat w)
+        , height (String.fromFloat h)
+        , viewBox <| "0 0 " ++ String.fromFloat w ++ " " ++ String.fromFloat h
+        , style "border: 1px dashed black; display: block"
         ]
         [ polyline
-            [ Svg.Attributes.points <| .path <| stateToSvgPath state
+            [ Svg.Attributes.points <| .path <| stateToSvgPath state w h
             , stroke "black"
             , fill "none"
             ]
@@ -135,28 +187,20 @@ type alias Drawing =
     }
 
 
-scale =
-    1.0
-
-
 size =
-    120.0
+    1200.0
 
 
-idp =
-    (size / scale) / 2
-
-
-initialPosition =
-    Position idp idp
+initialPosition w h =
+    Position (w / 2) (h / 2)
 
 
 posToStr pos =
     String.fromFloat pos.x ++ " " ++ String.fromFloat pos.y
 
 
-stateToSvgPath state =
-    List.foldl stepToPath (Drawing (posToStr initialPosition) initialPosition 0) state
+stateToSvgPath state w h =
+    List.foldl stepToPath (Drawing (posToStr <| initialPosition w h) (initialPosition w h) 0) state
 
 
 stepToPath : Step -> Drawing -> Drawing
@@ -173,10 +217,10 @@ stepToPath step drawing =
     in
     case step of
         L ->
-            { drawing | deg = drawing.deg + 90 }
+            { drawing | deg = drawing.deg - 90 }
 
         R ->
-            { drawing | deg = drawing.deg - 90 }
+            { drawing | deg = drawing.deg + 90 }
 
         D ->
             Drawing newPath newPos drawing.deg
@@ -210,10 +254,10 @@ stepToString step =
             "S"
 
 
-rule1 step =
+makeRule state step =
     case step of
         D ->
-            [ D, D, L, D, L, D ]
+            state
 
         _ ->
             [ step ]
