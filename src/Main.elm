7ca98343437c383ccb3d3d8c64e3e6712df12ed9
José Rodrigues <jyrodrigues@gmail.com>
2020-01-31 07:42:51 -0300

    Add URL handling, now the URL includes the composition

--- src/Main.elm
+++ src/Main.elm
@@ -3,6 +3,7 @@ port module Main exposing (main)
 import Browser
 import Browser.Dom exposing (Element)
 import Browser.Events
+import Browser.Navigation as Nav
 import Colors exposing (Color, offWhite, toCssColor)
 import Css
     exposing
@@ -63,6 +64,8 @@ import LSystem.Draw
         )
 import ListExtra exposing (pairExec, pairMap)
 import Task
+import Url
+import Url.Parser as Parser exposing ((</>), Parser, oneOf, s, string)
 
 
 port saveStateToLocalStorage : Encode.Value -> Cmd msg
@@ -97,6 +100,10 @@ type alias Model =
 
     -- Browser Focus
     , focus : Focus
+
+    -- Url
+    , url : Url.Url
+    , navKey : Nav.Key
     }
 
 
@@ -126,10 +133,17 @@ type
     | SetTurnAngle Float
       -- Focus
     | SetFocus Focus
+      -- URL
+    | UrlChanged Url.Url
+    | LinkClicked Browser.UrlRequest
 
 
 
 -- OTHER TYPES
+
+
+type Route
+    = Home Composition
 
 
 type alias Flags =
@@ -158,53 +172,77 @@ type Polygon
 
 main : Program Flags Model Msg
 main =
-    Browser.element
+    Browser.application
         { init = init
+        , view = view
         , update = update
         , subscriptions = subscriptions
-        , view = view
+        , onUrlChange = UrlChanged
+        , onUrlRequest = LinkClicked
         }
 
 
-init : Flags -> ( Model, Cmd Msg )
-init savedModel =
-    let
-        getImgDivPosition =
-            Task.attempt GotImgDivPosition (Browser.Dom.getElement "mainImg")
-    in
-    case Decode.decodeValue modelDecoder savedModel of
-        Ok model ->
-            ( model, getImgDivPosition )
-
-        Err err ->
+init : Flags -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
+init savedModel url navKey =
+    case parseUrlToRoute url of
+        -- Got a valid Composition from the URL
+        Just (Home composition) ->
             let
-                _ =
-                    Debug.log "Debug.log - Error while decoding localStorage" err
-
                 model =
-                    expandMinimalModel squareComposition Colors.darkGray Colors.defaultGreen 90 1 0 0
+                    basicModelFrom composition url navKey
             in
-            ( model
-            , Cmd.batch
-                [ saveStateToLocalStorage (encodeModel model)
-                , getImgDivPosition
-                ]
-            )
+            ( { model | editingIndex = LCore.length model.composition - 1 }, getImgDivPosition )
+
+        Nothing ->
+            case Decode.decodeValue modelDecoder savedModel of
+                -- Got a valid composition from localStorage
+                Ok modelWithoutUrlAndKey ->
+                    let
+                        model =
+                            modelWithoutUrlAndKey url navKey
+                    in
+                    ( model
+                    , Cmd.batch
+                        [ replaceUrl navKey model.composition
+                        , getImgDivPosition
+                        ]
+                    )
+
+                -- Didn't found a valid composition: default to squareComposition
+                Err err ->
+                    let
+                        _ =
+                            Debug.log "Debug.log - Error while decoding localStorage" err
+
+                        model =
+                            basicModelFrom squareComposition url navKey
+                    in
+                    ( model
+                    , Cmd.batch
+                        [ saveStateToLocalStorage (encodeModel model)
+                        , replaceUrl navKey model.composition
+                        , getImgDivPosition
+                        ]
+                    )
 
 
 
 -- VIEW
 
 
-view : Model -> Html.Html Msg
+view : Model -> Browser.Document Msg
 view model =
-    div
-        [ css [ width (pct 100), height (pct 100) ] ]
-        [ controlPanel model
-        , transformsList model
-        , mainImg model
+    { title = "Generative Art"
+    , body =
+        [ div
+            [ css [ width (pct 100), height (pct 100) ] ]
+            [ controlPanel model
+            , transformsList model
+            , mainImg model
+            ]
+            |> toUnstyled
         ]
-        |> toUnstyled
+    }
 
 
 controlPanel : Model -> Html Msg
@@ -451,89 +489,128 @@ fixedDiv attrs children =
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    (\newModel ->
-        ( newModel
-        , saveStateToLocalStorage (encodeModel newModel)
-        )
-    )
-    <|
-        case Debug.log "msg" msg of
-            ResetDrawing ->
-                -- TODO: Change this hacky implementation; add basePolygon to localStorage; and add a button to clear
-                -- localStorage.
-                updateCompositionBaseAndAngle
-                    { model | composition = squareComposition }
-                    model.basePolygon
+    case msg of
+        LinkClicked urlRequest ->
+            case urlRequest of
+                Browser.Internal url ->
+                    ( model, Nav.replaceUrl model.navKey (Url.toString url) )
 
-            Iterate editingIndex ->
-                iterate model editingIndex
+                Browser.External href ->
+                    ( model, Nav.load href )
 
-            Deiterate ->
-                deiterate model
+        UrlChanged url ->
+            ( model
+            , if
+                Maybe.map ((++) "#") (Maybe.andThen Url.percentDecode url.fragment)
+                    == Just (compositionToUrlString model.composition)
+              then
+                Cmd.none
 
-            KeyPress keyString ->
-                processKey model keyString
+              else
+                replaceUrl model.navKey model.composition
+            )
 
-            -- also, see `scaleAbout` in https://github.com/ianmackenzie/elm-geometry-svg/blob/master/src/Geometry/Svg.elm
-            -- and later check https://package.elm-lang.org/packages/ianmackenzie/elm-geometry-svg/latest/
-            Zoom _ deltaY _ mousePos ->
-                applyZoom deltaY mousePos model
+        -- TODO Don't save to localStorage neither replaceUrl on every msg!
+        {--TODO
+            Maybe create a type like UrlMsg = LinkClicked | UrlChanged | AppMsg Msg
+            then remove wildcard here and on updateModel.
+        --}
+        _ ->
+            (\newModel ->
+                ( newModel
+                , Cmd.batch
+                    [ saveStateToLocalStorage (encodeModel newModel)
+                    , replaceUrl newModel.navKey newModel.composition
+                    ]
+                )
+            )
+            <|
+                updateModel msg model
 
-            SetEditingIndex index ->
-                { model | editingIndex = index }
 
-            DropFromState index ->
-                { model
-                    | composition = LCore.dropBlockAtIndex index model.composition
-                    , editingIndex =
-                        if model.editingIndex >= index then
-                            model.editingIndex - 1
+updateModel : Msg -> Model -> Model
+updateModel msg model =
+    case Debug.log "msg" msg of
+        ResetDrawing ->
+            -- TODO: Change this hacky implementation; add basePolygon to localStorage; and add a button to clear
+            -- localStorage.
+            updateCompositionBaseAndAngle
+                { model | composition = squareComposition }
+                model.basePolygon
 
-                        else
-                            model.editingIndex
-                }
+        Iterate editingIndex ->
+            iterate model editingIndex
 
-            SetBackgroundColor color ->
-                { model | backgroundColor = color }
+        Deiterate ->
+            deiterate model
 
-            SetStrokeColor color ->
-                { model | strokeColor = color }
+        KeyPress keyString ->
+            processKey model keyString
 
-            SetTurnAngle turn ->
-                { model | turnAngle = turn }
+        -- also, see `scaleAbout` in https://github.com/ianmackenzie/elm-geometry-svg/blob/master/src/Geometry/Svg.elm
+        -- and later check https://package.elm-lang.org/packages/ianmackenzie/elm-geometry-svg/latest/
+        Zoom _ deltaY _ mousePos ->
+            applyZoom deltaY mousePos model
 
-            PanStarted pos ->
-                { model | panStarted = True, lastPos = pos }
+        SetEditingIndex index ->
+            { model | editingIndex = index }
 
-            PanEnded ->
-                { model | panStarted = False }
+        DropFromState index ->
+            { model
+                | composition = LCore.dropBlockAtIndex index model.composition
+                , editingIndex =
+                    if model.editingIndex >= index then
+                        model.editingIndex - 1
 
-            MouseMoved pos ->
-                { model
-                    | translate =
-                        pos
-                            |> pairExec (-) model.lastPos
-                            |> pairExec (+) model.translate
-                    , lastPos = pos
-                }
+                    else
+                        model.editingIndex
+            }
 
-            SetFocus focus ->
-                { model | focus = focus }
+        SetBackgroundColor color ->
+            { model | backgroundColor = color }
 
-            BasePolygonChanged polygon ->
-                updateCompositionBaseAndAngle model polygon
+        SetStrokeColor color ->
+            { model | strokeColor = color }
 
-            GotImgDivPosition result ->
-                case result of
-                    Ok data ->
-                        let
-                            { x, y, width, height } =
-                                data.element
-                        in
-                        { model | imgDivCenter = ( x + width / 2, y + height / 2 ), imgDivStart = ( x, y ) }
+        SetTurnAngle turn ->
+            { model | turnAngle = turn }
 
-                    Err _ ->
-                        model
+        PanStarted pos ->
+            { model | panStarted = True, lastPos = pos }
+
+        PanEnded ->
+            { model | panStarted = False }
+
+        MouseMoved pos ->
+            { model
+                | translate =
+                    pos
+                        |> pairExec (-) model.lastPos
+                        |> pairExec (+) model.translate
+                , lastPos = pos
+            }
+
+        SetFocus focus ->
+            { model | focus = focus }
+
+        BasePolygonChanged polygon ->
+            updateCompositionBaseAndAngle model polygon
+
+        GotImgDivPosition result ->
+            case result of
+                Ok data ->
+                    let
+                        { x, y, width, height } =
+                            data.element
+                    in
+                    { model | imgDivCenter = ( x + width / 2, y + height / 2 ), imgDivStart = ( x, y ) }
+
+                Err _ ->
+                    model
+
+        -- TODO remove this
+        _ ->
+            model
 
 
 processKey : Model -> String -> Model
@@ -618,7 +695,7 @@ applyZoom deltaY mousePos model =
 updateCompositionBaseAndAngle : Model -> Polygon -> Model
 updateCompositionBaseAndAngle model polygon =
     let
-        updateModel stateBase turnAngle =
+        resetModel stateBase turnAngle =
             { model
                 | composition = LCore.changeBase stateBase model.composition
                 , turnAngle = turnAngle
@@ -627,16 +704,25 @@ updateCompositionBaseAndAngle model polygon =
     in
     case polygon of
         Triangle ->
-            updateModel [ D, L, D, L, D ] 120
+            resetModel [ D, L, D, L, D ] 120
 
         Square ->
-            updateModel [ D, L, D, L, D, L, D ] 90
+            resetModel [ D, L, D, L, D, L, D ] 90
 
         Pentagon ->
-            updateModel [ D, L, D, L, D, L, D, L, D ] 72
+            resetModel [ D, L, D, L, D, L, D, L, D ] 72
 
         Hexagon ->
-            updateModel [ D, L, D, L, D, L, D, L, D, L, D ] 60
+            resetModel [ D, L, D, L, D, L, D, L, D, L, D ] 60
+
+
+
+-- COMMANDS
+
+
+getImgDivPosition : Cmd Msg
+getImgDivPosition =
+    Task.attempt GotImgDivPosition (Browser.Dom.getElement "mainImg")
 
 
 
@@ -656,11 +742,59 @@ subscriptions model =
 
 
 
+-- URL
+
+
+parseUrlToRoute : Url.Url -> Maybe Route
+parseUrlToRoute url =
+    -- Based on the RealWorld Example we treats the fragment like a path.
+    -- This makes it *literally* the path, so we can proceed
+    -- with parsing as if it had been a normal path all along.
+    -- TODO change to query param!
+    { url | path = Maybe.withDefault "" url.fragment, fragment = Nothing }
+        |> Parser.parse urlParser
+
+
+urlParser : Parser (Route -> a) a
+urlParser =
+    -- Can be transformed into multiple Routes like this:
+    -- oneOf
+    -- [ Parser.map Blank Parser.top
+    Parser.map Home
+        (Parser.custom "COMPOSITION" <|
+            Url.percentDecode
+                >> Maybe.andThen (Decode.decodeString LCore.compositionDecoder >> Result.toMaybe)
+        )
+
+
+compositionToUrlString : Composition -> String
+compositionToUrlString composition =
+    composition
+        |> LCore.encodeComposition
+        |> Encode.encode 0
+        -- goes in front, like "#[['D','R'],['D']]"
+        |> (++) "#"
+
+
+replaceUrl : Nav.Key -> Composition -> Cmd msg
+replaceUrl key composition =
+    {--
+            Note about Nav.replaceUrl: Browsers may rate-limit this function by throwing an
+            exception. The discussion here suggests that the limit is 100 calls per 30 second
+            interval in Safari in 2016. It also suggests techniques for people changing the
+            URL based on scroll position.
+
+            https://package.elm-lang.org/packages/elm/browser/latest/Browser-Navigation#replaceUrl
+        --}
+    Nav.replaceUrl key (compositionToUrlString composition)
+
+
+
 -- MODEL STUFF
 
 
-expandMinimalModel : Composition -> Color -> Color -> Float -> Float -> Float -> Float -> Model
-expandMinimalModel state bgColor strokeColor turnAngle scale translateX translateY =
+expandMinimalModel : Composition -> Color -> Color -> Float -> Float -> Float -> Float -> Url.Url -> Nav.Key -> Model
+expandMinimalModel state bgColor strokeColor turnAngle scale translateX translateY url navKey =
     Model
         -- Aplication heart
         state
@@ -681,6 +815,14 @@ expandMinimalModel state bgColor strokeColor turnAngle scale translateX translat
         turnAngle
         -- Browser Focus
         KeyboardEditing
+        -- Url
+        url
+        navKey
+
+
+basicModelFrom : Composition -> Url.Url -> Nav.Key -> Model
+basicModelFrom composition url navKey =
+    expandMinimalModel composition Colors.darkGray Colors.defaultGreen 90 1 0 0 url navKey
 
 
 squareComposition : Composition
@@ -690,7 +832,7 @@ squareComposition =
 
 {-| Model encoder and decoder
 -}
-modelDecoder : Decoder Model
+modelDecoder : Decoder (Url.Url -> Nav.Key -> Model)
 modelDecoder =
     Decode.map7 expandMinimalModel
         (Decode.field "state" LCore.compositionDecoder)
