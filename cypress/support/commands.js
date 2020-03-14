c3775a7e16e06eb4f4b61b927b2a0b4ea45cd280
José Rodrigues <jyrodrigues@gmail.com>
2020-03-14 19:56:42 -0300

    Add cypress command example setting localStorage before test run

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,26 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+//
+
+// How to set localstorage before tests (to be used with localstorage versioning tests and visual diff
+//
+//
+// https://stackoverflow.com/questions/50820732/in-cypress-set-a-token-in-localstorage-before-test
+
+Cypress.Commands.add('loginWithJWT', () => {
+  cy.request({
+    method: 'POST',
+    url: 'http://localhost:3000/api/users/login',
+    body: {
+      user: {
+        email: 'jake@jake.jake',
+        password: 'jakejake',
+      }
+    }
+  })
+  .then((resp) => {
+    window.localStorage.setItem('jwt', resp.body.user.token)
+  })
+
+})
